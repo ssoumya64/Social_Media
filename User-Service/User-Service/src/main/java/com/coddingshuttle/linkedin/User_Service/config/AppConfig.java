babaedc69cabfee6731586f551356f346bf9ac31
ssoumya64 <sahoosoumya4862@gmail.com>
2025-03-15 17:09:24 +0530

    Added the structure and some Entity

---
+++ User-Service/User-Service/src/main/java/com/coddingshuttle/linkedin/User_Service/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.coddingshuttle.linkedin.User_Service.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelmapper(){
+        return new ModelMapper();
+    }
+}
